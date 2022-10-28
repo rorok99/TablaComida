93dce811a58618238fcc1e5a70a5d5db8f429517
Robespier <robespiergtl3@gmail.com>
2022-10-28 00:42:32 -0400

    logic-of-register

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const precios = [12, 23, 7, 19, 10];
 
 $(() => {
+  let isLoggedIn = false;
   const $element = $("#table tbody tr");
   const $buy = $("#buy");
   $element.on("click", function (e) {
@@ -14,6 +15,22 @@ $(() => {
   });
   $buy.on("click", function (e) {
     console.log($buy.find("span").text());
+  });
+
+  $("#submit").on("click", function (e) {
+    let passValue = $("#password").val();
+    let repeatPassValue = $("#repeatpassword").val();
+    let numOfChars = (passValue.length >= 8);
+    let isThereAnUpperChar = false;
+    for (let i = 0; i < passValue.length; i++)
+      if (passValue[i] == passValue[i].toUpperCase()) {
+        isThereAnUpperChar = true;
+        console.log(passValue[i]);
+        break;
+      }
+    if (numOfChars && isThereAnUpperChar && passValue == repeatPassValue) {
+      isLoggedIn = true;
+    }
   });
 
 });
